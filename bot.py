70a39fe6c24af7bc8dec01b2c9e6a973127c17ef
Gustavo <94932251+gugasth@users.noreply.github.com>
2023-09-14 22:17:54 -0300

    revisão documentação

--- bot.py
+++ bot.py
@@ -2,16 +2,16 @@ import tweepy
 import os
 from dotenv import load_dotenv
 
-# Carregue as variáveis de ambiente a partir de um arquivo .env
+# carregando as variáveis de ambiento do .env
 load_dotenv()
 
-# Configure as chaves e tokens do Twitter
+# acessando as chaves de api do /env
 consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
 consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET")
 access_token = os.getenv("TWITTER_ACCESS_TOKEN")
 access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
-# Autentique-se no Twitter
+# autenticação
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
@@ -20,4 +20,4 @@ try:
     api.verify_credentials()
     print("Authentication OK")
 except:
-    print("Error during authentication")
+    print("Authentication error")
