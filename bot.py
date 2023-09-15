fb3c70b4b7e12a148e12bef596974bdf13f85f7f
Gustavo <94932251+gugasth@users.noreply.github.com>
2023-09-14 22:08:50 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+import tweepy
+import os
+from dotenv import load_dotenv
+
+# Carregue as variáveis de ambiente a partir de um arquivo .env
+load_dotenv()
+
+# Configure as chaves e tokens do Twitter
+consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
+consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET")
+access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+
+# Autentique-se no Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
